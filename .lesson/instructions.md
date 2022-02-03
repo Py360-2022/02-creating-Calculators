6045887f4692e0ad30da4bde337ed4f2e7d95cb3
P Campbell <triciamontreal@gmail.com>
2022-02-03 16:25:38 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,14 @@
+# Instructions  
+
+You may not complete this during the lab time, you can come back to it.
+
+Use the slides (on moodle) or [here](https://collegedawson-my.sharepoint.com/personal/pcampbell_dawsoncollege_qc_ca/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fpcampbell%5Fdawsoncollege%5Fqc%5Fca%2FDocuments%2F2022%2D360%2Fmoodle%2Dshared%2Ddirectory) and [code examples on github](https://github.com/campbe13/py360-2021/tree/main/lecture-code/02)
+
+1. create an algorithm: think about the problem, create a flowchart or write in point form the steps you will use 
+1. use iterative development ___(code a bit, test it, do the next bit...)___
+1. use comments for code you want to keep but not remove (ex for debugging)
+1. use print statements to see what is in your variables, if you are not sure (ex for debugging)
+5. if you do not finish today you can logoff and come back to complete it
+1. when you have finished all three you can `submit`   you do not need to submit, do so if you want feedback from me.  (some time next week)
+
+** The coding  instructions are in the main.py file, modify that directly **
---
+++ main.py
@@ -0,0 +1,30 @@
+# description of the script here   <<<< change this
+# for one or two lines <<< change this
+# your name here <<<< change this
+# todays date <<<< change this
+
+# Perimeter Calculator​
+''''
+Write the code that asks the user to enter 2 numbers representing the width and length of a rectangle. The program calculates and displays the perimeter of the rectangle. ​
+
+write down test data + calculate results  & run your code a few times with that data
+write code below the print statement
+'''
+print("perimiter calculator")
+
+# Restaurant Tip Calculator ​
+'''
+Write the code that asks the user to enter the price of a meal at a restaurant (remember those  ?? ). The program calculates the amount of the tip to be paid at 18%. The tip and total price are then displayed separately.​
+
+write down test data + calculate results  & run your code a few times with that data
+write code below 
+'''
+print("perimiter calculator")
+
+# Volume and Surface Area Calculator ​
+''''
+Write the code that asks the user to enter 3 numbers representing the height, width and length of a cuboid. The program calculates and displays the volume and total surface area of the cuboid. ​
+
+write down test data + calculate results  & run your code a few times with that data
+write code below
+'''
